542fb410b933e83b5ba59f3c13cc9894717d5500
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-10-19 12:51:12 +0100

    contact

--- contact.php
+++ contact.php
@@ -84,17 +84,17 @@
                             <p class="mp-b-1">Please indicate your expected timescale for this project:</p>
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="asap"><input type="radio" name="timescale"   value="asap"  class="radio-input" id="asap"checked>ASAP</label>
+                                <label class="radio-label" for="asap"><input type="radio" name="timescale"   value="asap"  class="radio-input" id="timescale"checked>ASAP</label>
                             </div>
                             
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="3to6"><input type="radio" name="timescale"   value="3to6" class="radio-input" id="3to6">3 to 6 Months</label>
+                                <label class="radio-label" for="3to6"><input type="radio" name="timescale"   value="3to6" class="radio-input" id="timescale">3 to 6 Months</label>
                             </div>
 
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="6to9"><input type="radio" name="timescale"   value="6to9" class="radio-input" id="6to9">6 to 9 Months</label>
+                                <label class="radio-label" for="6to9"><input type="radio" name="timescale"   value="6to9" class="radio-input" id="timescale">6 to 9 Months</label>
                             </div>
                           
                         </div>
@@ -215,9 +215,9 @@
                                     phone: $('#phone').val(),
                                     message: $('#message').val(),
                                     validation: $('.g-recaptcha-response').val(),
-                                    newwebsite: $('newwebsite').val(),
-                                    requirements: $('requirements').val(),
-                                    timescale: $('timescale').val(),
+                                    
+                                    
+                                    timescale: $('#timescale').val(),
                                     currentwebsite: $('#currentwebsite').val()
 
 

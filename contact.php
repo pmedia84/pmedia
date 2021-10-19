087ed271eb9d9c97e220f0174fe4a565d98d4ed3
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-10-19 08:57:52 +0100

    starting radio boxes

--- contact.php
+++ contact.php
@@ -84,17 +84,17 @@
                             <p class="mp-b-1">Please indicate your expected timescale for this project:</p>
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="asap"><input type="radio" class="radio-input" id="asap" name="timescale[]" value="asap" checked>ASAP</label>
+                                <label class="radio-label" for="asap"><input type="radio" class="radio-input" id="asap" name="timescale" value="asap" checked>ASAP</label>
                             </div>
                             
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="3to6"><input type="radio" class="radio-input" id="3to6" name="timescale[]" value="3to6">3 to 6 Months</label>
+                                <label class="radio-label" for="3to6"><input type="radio" class="radio-input" id="3to6" name="timescale" value="3to6">3 to 6 Months</label>
                             </div>
 
                             <div class="form-radio">
                                 
-                                <label class="radio-label" for="6to9"><input type="radio" class="radio-input" id="6to9" name="timescale[]" value="6to9">6 to 9 Months</label>
+                                <label class="radio-label" for="6to9"><input type="radio" class="radio-input" id="6to9" name="timescale" value="6to9">6 to 9 Months</label>
                             </div>
                           
                         </div>

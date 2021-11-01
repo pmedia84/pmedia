51f427bf632bebc113d9459dbeba3471c5a35bff
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-01 13:27:40 +0000

    update to services page

---
+++ services.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Parrot Media : Services</title>
+
+    <?php include("inc/nav.inc.php"); ?>
+
+
+
+
+
+
+
+    <?php include("inc/footer.inc.php"); ?>

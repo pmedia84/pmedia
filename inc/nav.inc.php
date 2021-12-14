83bf005dd8961d814faf4bbda0303f58d28f40e2
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-12-14 10:35:49 +0000

    adding fb pixel

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -22,7 +22,23 @@
 <link rel="stylesheet" href="css/styles.css">
 <script defer src="https://kit.fontawesome.com/3318fdaaaf.js" crossorigin="anonymous"></script>
 
-
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1036906363538583');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1036906363538583&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 </head>
 
 <body>

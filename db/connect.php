6825dd2f87964ffc9af4671b546dea7f6550b4d4
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2022-06-10 09:42:34 +0100

    4

--- db/connect.php
+++ db/connect.php
@@ -7,3 +7,8 @@ $db = 'parrotme_news';
 
 $conn = mysqli_connect($server, $user,$pass, $db);
 ?>
+
+<!-- $server = 'localhost';
+$user = 'parrotme_admin';
+$pass = 'Krb833908!';
+$db = 'parrotme_news'; -->
